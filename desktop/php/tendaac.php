e71c1bab2df0cd1c29552a25d8985527abbaade8
Flobul <campbell@MacBook-Air-de-Campbell.local>
2020-01-10 18:02:18 +0100

    Ajout sauvegarde/restaure page accueil

--- desktop/php/tendaac.php
+++ desktop/php/tendaac.php
@@ -21,6 +21,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
             </ul>
         </div>
     </div>
+
     <div class="col-lg-9 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
         <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
         <div class="eqLogicThumbnailContainer">
@@ -68,113 +69,144 @@ $eqLogics = eqLogic::byType($plugin->getId());
             ?>
         </div>
     </div>
-    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-        <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-		<a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-		<a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
-		<ul class="nav nav-tabs" role="tablist">
-			<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
-			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
-			<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
-		</ul>
-		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
-			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
-                <form class="form-horizontal">
-                    <fieldset>
-                        <legend>
-                           <i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}
-                           <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
-                        </legend>
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label">{{Nom du routeur Tenda}}</label>
-                            <div class="col-lg-3">
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom du routeur Tenda}}"/>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label" >{{Objet parent}}</label>
-                            <div class="col-lg-3">
-                                <select class="form-control eqLogicAttr" data-l1key="object_id">
-                                    <option value="">{{Aucun}}</option>
-                                    <?php
-                                    foreach (jeeObject::all() as $object) {
-                                        echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>'."\n";
-                                    }
-                                    ?>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label">{{Catégorie}}</label>
-                            <div class="col-lg-8">
-                                <?php
-                                foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
-                                    echo '<label class="checkbox-inline">'."\n";
-                                    echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
-                                    echo '</label>'."\n";
-                                }
-                                ?>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                          <label class="col-sm-2 control-label" ></label>
-                            <div class="col-sm-10">
-                            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>Activer</label>
-                            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>Visible</label>
-                            </div>
-                            <a class="btn btn-default" id="bt_goWebpage" title='{{Accéder à la page web}}'><i class="fa fa-cogs"></i></a>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label">{{IP du routeur Tenda}}</label>
-                            <div class="col-lg-3">
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ip"/>
-                            </div>
-                        </div>
-                        <div class="form-group">
-							<label class="col-lg-2 control-label">{{Mot de passe du routeur Tenda}}
-								<sup>
-									<i class="fa fa-question-circle tooltips" title="Saisissez le mot de passe d'accès au routeur.
-			Laisse le champ vide si vous n'avez pas de mot de passe." style="font-size : 1em;color:grey;"></i>
-								</sup>
-							</label>
-                            <div class="col-lg-3">
-                                <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="password"/>
-                            </div>
-                        </div>
-                    </fieldset>
-                </form>
-			<div class="col-sm-6">
-					<form class="form-horizontal">
-						<legend>{{Configuration du plugin}}
-						</legend>
-						<fieldset>
-							<div class="form-group">
-								<label class="col-sm-4 control-label" >
-									{{Intervalle de rafraîchissement des informations}}
-									<sup>
-										<i class="fa fa-question-circle tooltips" title="Récupération des informations par envoi de la commande 'Refresh' à intervalle choisi.
-La commande est envoyée toutes les minutes, 5 minutes, 10 minutes, 15 minutes, 30 minutes..." style="font-size : 1em;color:grey;"></i>
-									</sup>
-								</label>
-								<div class="col-sm-7 input-group">
-									<select class="eqLogicAttr form-control input-sm" data-l1key="configuration" data-l2key="RepeatCmd">
-										<option value="">{{Non}}</option>
-										<option value="cron">{{Toutes les minutes}}</option>
-										<option value="cron5">{{Toutes les 5 minutes}}</option>
-										<option value="cron10">{{Toutes les 10 minutes}}</option>
-										<option value="cron15">{{Toutes les 15 minutes}}</option>
-										<option value="cron30">{{Toutes les 30 minutes}}</option>
-										<option value="cronHourly">{{Toutes les heures}}</option>
-									</select>
-								</div>
-							</div>
-						</fieldset>
-					</form>
-			</div>
-			</div>
 
-        	<div role="tabpanel" class="tab-pane" id="commandtab">
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+      <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+      <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+      <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+      <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+        <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+        <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+      </ul>
+      <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+        <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+          <div class="col-sm-6">
+          <form class="form-horizontal">
+            <fieldset>
+              <legend>
+                <i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}
+                <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
+              </legend>
+
+              <div class="form-group">
+                <label class="col-sm-3 control-label">{{Nom du routeur Tenda}}</label>
+                <div class="col-sm-7">
+                  <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                  <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom du routeur Tenda}}"/>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                <div class="col-sm-7">
+                  <select class="form-control eqLogicAttr" data-l1key="object_id">
+                    <option value="">{{Aucun}}</option>
+                    <?php
+                    foreach (jeeObject::all() as $object) {
+                      echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>'."\n";
+                    }
+                    ?>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-3 control-label">{{Catégorie}}</label>
+                <div class="col-sm-7">
+                  <?php
+                  foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+                    echo '<label class="checkbox-inline">'."\n";
+                    echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+                    echo '</label>'."\n";
+                  }
+                  ?>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-3 control-label" ></label>
+                <div class="col-sm-5">
+                  <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>Activer</label>
+                  <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>Visible</label>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <legend>{{Configuration}}
+                </legend>
+                <a class="btn btn-default" id="bt_goWebpage" title='{{Accéder à la page web}}'><i class="fa fa-cogs"></i></a>
+                <label class="col-sm-4 control-label">{{IP du routeur Tenda}}</label>
+                <div class="col-sm-7">
+                  <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ip"/>
+                </div>
+              </div>
+
+
+              <div class="form-group">
+                <label class="col-sm-4 control-label">{{Mot de passe du routeur Tenda}}
+                  <sup>
+                    <i class="fa fa-question-circle tooltips" title="Saisissez le mot de passe d'accès au routeur.
+Laisse le champ vide si vous n'avez pas de mot de passe." style="font-size : 1em;color:grey;"></i>
+                  </sup>
+                </label>
+                <div class="col-sm-7">
+                  <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="password"/>
+                </div>
+                </div>
+
+
+			<div class="form-group">
+                <label class="col-sm-4 control-label">{{Rafraîchissement des informations}}
+                	<sup>
+                    <i class="fa fa-question-circle tooltips" title="Réception des informations à intervalle selectionné.
+  La commande est envoyée toutes les minutes, 5 minutes, 10 minutes, 15 minutes, 30 minutes..." style="font-size : 1em;color:grey;"></i>
+                	</sup>
+               	</label>
+               	<div class="col-sm-7">
+					<select class="eqLogicAttr form-control input-sm" data-l1key="configuration" data-l2key="RepeatCmd">
+						<option value="">{{Non}}</option>
+						<option value="cron">{{Toutes les minutes}}</option>
+						<option value="cron5">{{Toutes les 5 minutes}}</option>
+						<option value="cron10">{{Toutes les 10 minutes}}</option>
+						<option value="cron15">{{Toutes les 15 minutes}}</option>
+						<option value="cron30">{{Toutes les 30 minutes}}</option>
+						<option value="cronHourly">{{Toutes les heures}}</option>
+					</select>
+        </div>
+      </div>
+			</fieldset>
+			</form>
+		</div>
+
+		<div class="col-lg-6">
+			<form class="form-horizontal">
+				<legend>Sauvegardes</legend>
+					<fieldset>
+            <div class="form-group">
+              <label class="col-xs-12"><i class="fas fa-tape"></i> Sauvegardes disponibles</label>
+              <div class="col-xs-12">
+                <select class="form-control" id="sel_restoreBackup"><option value="/var/www/html/core/class/../../backup/backup-MaisonFlorine-4.0.38-2020-01-10-03h43.tar.gz">backup-MaisonFlorine-4.0.38-2020-01-10-03h43.tar.gz</option><option value="/var/www/html/core/class/../../backup/backup-MaisonFlorine-4.0.38-2020-01-09-03h43.tar.gz">backup-MaisonFlorine-4.0.38-2020-01-09-03h43.tar.gz</option><option value="/var/www/html/core/class/../../backup/backup-MaisonFlorine-4.0.37-2020-01-08-13h55.tar.gz">backup-MaisonFlorine-4.0.37-2020-01-08-13h55.tar.gz</option></select>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-sm-6 col-xs-12">
+                <a class="btn btn-danger" id="bt_removeBackupTenda" style="width:100%;"><i class="far fa-trash-alt"></i> Supprimer la sauvegarde</a>
+              </div>
+              <div class="col-sm-6 col-xs-12">
+                <a class="btn btn-warning" id="bt_restoreBackupTenda" style="width:100%;"><i class="fas fa-sync fa-spin" style="display:none;"></i> <i class="far fa-file"></i> Restaurer la sauvegarde</a>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-6 col-xs-12">
+                <a class="btn btn-success" id="bt_downloadBackupTenda" style="width:100%;"><i class="fas fa-cloud-download-alt"></i> Télécharger la sauvegarde</a>
+              </div>
+           </div>
+
+			</fieldset>
+			</form>
+		</div>
+  </div>
+
+        <div role="tabpanel" class="tab-pane" id="commandtab">
                 <table id="table_cmd" class="table table-bordered table-condensed">
                     <thead>
                         <tr>
